6f338c96d414967a7c1ef219ea1044adc7bd84d7
Boris Barroso <boriscyber@gmail.com>
2009-11-11 10:14:05 -0400

    Actualizando migraciones

--- db/migrate/20091028201135_add_archivos_adjuntos.rb
+++ db/migrate/20091028201135_add_archivos_adjuntos.rb
@@ -2,10 +2,12 @@ class AddArchivosAdjuntos < ActiveRecord::Migration
   def self.up
     add_column :archivos, :archivo_excel_file_name, :string
     add_column :archivos, :archivo_excel_file_size, :integer
+    add_column :archivos, :archivo_excel_updated_at, :datetime
   end
 
   def self.down
     remove_column :archivos, :archivo_excel_file_name
     remove_column :archivos, :archivo_excel_file_size
+    remove_column :archivos, :archivo_excel_updated_at
   end
 end
--- db/migrate/20091109164356_add_archivos_fecha_modificacion.rb
+++ db/migrate/20091109164356_add_archivos_fecha_modificacion.rb
@@ -2,11 +2,9 @@
 class AddArchivosFechaModificacion < ActiveRecord::Migration
   def self.up
     add_column :archivos, :fecha_modificacion, :datetime
-    add_column :archivos, :archivo_excel_updated_at, :datetime
   end
 
   def self.down
     remove_column :archivos, :fecha_modificacion
-    remove_column :archivos, :archivo_excel_updated_at
   end
 end
