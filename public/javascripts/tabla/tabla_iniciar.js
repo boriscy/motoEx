3888e7d8d9985a23346a60bf58f0fd4f88ae7c50
nardhar <nardhar.mure@gmail.com>
2010-02-04 01:06:53 -0400

    iniciando ayuda

--- public/javascripts/tabla/tabla_iniciar.js
+++ public/javascripts/tabla/tabla_iniciar.js
@@ -3,6 +3,12 @@
  */
 $(document).ready(function() {
 
+    // para que abra un popup al hacer click en un enlace de la clase .ventana-ayuda
+    $('.ventana-ayuda').click(function(){
+        window.open(this.href, this.target, 'width=' + 400 + ',height=' + 300);
+        //openWindow(, , 400, 300);
+        return false;
+    });
     // Variable global para sabe cuando shift es presionada
     shift = false;
     $(window).keydown( function(e) {
